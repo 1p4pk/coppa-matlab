2263bc15392a8e0802096ca0b4c7cad4600b89b4
JeBr <j_brun17@uni-muenster.de>
2018-08-03 17:52:42 -0300

    prediction WIP engine in question

--- coppa/prediction.m
+++ coppa/prediction.m
@@ -10,7 +10,8 @@ function prediction = prediction(bnet, data, steps)
 
 ncases = length(data);
 
-engine = jtree_dbn_inf_engine(bnet);
+engine = bk_ff_hmm(bnet);
+engine = dbn_init_bel(engine);
 ss = bnet.nnodes_per_slice; %number of nodes in model
 onodes = 2:ss; % all but the first two (state and activity)
 nhd = bnet.node_sizes_slice(1,1); %node size of hidden state
@@ -19,8 +20,13 @@ prediction = cell(ncases);
 
 for j=1:ncases
     T = length(evidence{j}); %length of the case
-    for i=1:nhd
-          [engine, ll] = enter_evidence(engine, evidence{j});
-          marg = marginal_nodes(engine, i, T+steps); %calculate marginal nodes for hidden state i for steps into the future 
+    for i=1:T
+        if i>1
+            engine = dbn_update_bel(engine, evidence{j}(:,i-1:i));
+        else
+            engine = dbn_update_bel1(engine, evidence{j}(:,i));
+        end 
     end
+    engine = dbn_predict_bel(engine, 1);
+    m = dbn_marginal_from_bel(engine, 1);
 end
