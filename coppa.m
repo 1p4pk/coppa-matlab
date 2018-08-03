a696750de859c6affe449909237c573af3d75263
MaStFAU <so96vepi@fau.de>
2018-08-03 17:47:59 -0300

    Update

--- coppa.m
+++ coppa.m
@@ -8,14 +8,12 @@ addpath(genpath('./coppa/'));
 %%
 %Load data set
 %Input Required: only discrete attributes (including timestamp!)
-filename = './example/data.csv'; delimiter = ';'; timestamp_format = 'yyyy-MM-dd''T''HH:mm:ssXXX'; CaseID = 1; Activity = 2; Timestamp = 3;
-%filename = './example/bpi2013/VINST cases closed problems_COPPA.csv';delimiter = ';'; timestamp_format = 'yyyy-MM-dd''T''HH:mm:ssXXX'; CaseID = 1; Activity = 2; Timestamp = 3;
+%filename = './example/data.csv'; delimiter = ';'; timestamp_format = 'yyyy-MM-dd''T''HH:mm:ssXXX'; CaseID = 1; Activity = 2; Timestamp = 3;
+filename = './example/bpi2013/VINST cases closed problems_COPPA.csv';delimiter = ';'; timestamp_format = 'yyyy-MM-dd''T''HH:mm:ssXXX'; CaseID = 1; Activity = 2; Timestamp = 3;
 %filename = './example/sap/SAP_P2P_COPPA_FULL.csv'; delimiter = ','; timestamp_format = 'yyyy-MM-dd HH:mm:ss.SSSSSSS'; CaseID = 1; Activity = 2; Timestamp = 3;
-%filename = './example/bpi2012/bpi_2012.csv';delimiter = ','; timestamp_format = 'yyyy/MM/dd HH:mm:ss.SSS'; CaseID = 1; Activity = 2; Timestamp = 4;
 
 %Load Data
 data = import_csv(filename, delimiter); 
-load('mapping_table.mat');
 
 %Prepare Data
 [dataTraining dataTesting ns datn] = prepare_data(data,timestamp_format,CaseID,Timestamp,Activity,70); 
@@ -25,13 +23,15 @@ N = datn -1 + 1; % number of variables in one time slice. datn + 1 (for hidden s
 bnet = create_dbn(N,ns);
 %% 
 
-%bestbnet =  learning(bnet,N,dataTraining,10);
+bestbnet =  learning(bnet,N,dataTraining,10);
 
-load('bestbnet_allHVs.mat')
+%load('bestbnet_sap_p2p.mat');
 
+%% Draw Model
 G = bestbnet.dag;
 %draw_graph(G);
+%% 
 
-%pred = prediction(bestbnet, dataTesting);
+pred = prediction(bestbnet, dataTesting);
 
 mapped_value = map_to_name(2,2);
