f82b47a40761d8a7ddfc1b72cebe55cbcf22c5ca
MaStFAU <so96vepi@fau.de>
2018-08-27 16:03:33 +0200

    implemented specificity and sensitivity. check for correctness needed!

--- coppa/sensitivity.m
+++ coppa/sensitivity.m
@@ -1,6 +1,9 @@
 function score = sensitivity(prediction, real_value)
 %ACCURACY 
 %   Calculates prediction sensitivity
+%   TPR = TP/P
+%   As we have a multiclass problem we will assume each activity once as
+%   positive and calculate the weighted average in the end
 %
 % Input
 %%      prediction = predicted values
@@ -11,10 +14,24 @@ function score = sensitivity(prediction, real_value)
 
 assert(isequal(size(prediction),size(real_value)),'Length of prediction vector different from vector with real values');
 ncases = length(prediction);
-sensitivity = zeros(1,ncases);
-for i=1:ncases
-    if real_value{i} == prediction{i}
-        sensitivity(i) = 1;
+
+numberOfSymbols = max([real_value{1,:}]);
+sensitivity = zeros(1,numberOfSymbols);
+for j=1:numberOfSymbols
+    tp = 0;
+    p = 0;
+    for i=1:ncases
+        if real_value{i} == j
+            p = p + 1;
+            if real_value{i} == prediction{i}
+                tp = tp + 1;
+            end
+        end
+    end
+    if p>0
+        sensitivity(j) = tp/p;
+    else
+        sensitivity(j) = 0;
     end
 end
 score = mean(sensitivity);
--- coppa/specificity.m
+++ coppa/specificity.m
@@ -1,6 +1,9 @@
 function score = specificity(prediction, real_value)
 %ACCURACY 
 %   Calculates prediction specificity
+%   TNR = TN/N
+%   As we have a multiclass problem we will assume each activity once as
+%   positive and calculate the weighted average in the end
 %
 % Input
 %%      prediction = predicted values
@@ -11,10 +14,25 @@ function score = specificity(prediction, real_value)
 
 assert(isequal(size(prediction),size(real_value)),'Length of prediction vector different from vector with real values');
 ncases = length(prediction);
-specificity = zeros(1,ncases);
-for i=1:ncases
-    if real_value{i} == prediction{i}
-        specificity(i) = 1;
+
+numberOfSymbols = max([real_value{1,:}]);
+specificity = zeros(1,numberOfSymbols);
+
+for j=1:numberOfSymbols
+    tn = 0;
+    n = 0;
+    for i=1:ncases
+        if real_value{i} ~= j
+            n = n + 1;
+            if real_value{i} == prediction{i}
+                tn = tn + 1;
+            end
+        end
+    end
+    if n>0
+        specificity(j) = tn/n;
+    else
+        specificity(j) = 0;
     end
 end
 score = mean(specificity);
