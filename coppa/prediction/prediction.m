25afa4907e8821ca775fc1c0e4e5df9eb708d886
1l4pk <leonpapke@gmx.de>
2019-01-25 14:20:52 +0100

    Code improvement. Same calculation without having to create a static BN and do inference on it.

--- coppa/prediction/prediction.m
+++ coppa/prediction/prediction.m
@@ -33,36 +33,14 @@ for j=1:ncases
     end
     %enter evidence and choose filtering (smoothing is default)
     engine = enter_evidence(engine, evidenceToEnter, 'filter', 1);
-    %calculate marginals on the hidden node in t+steps
-    mS = marginal_nodes(engine, 1, T-1+steps);
+    %calculate marginals of the activity node in t+steps
+    mA = marginal_nodes(engine, 2, T-1+steps);
 
-    %create static bayesian network to do inference on observed node
-    %activity
-    bnet = mk_bnet(dbnet.intra, dbnet.node_sizes_slice, 'discrete', dbnet.dnodes_slice, 'observed', dbnet.observed);
-    for n=1:dbnet.nnodes_per_slice
-        bnet.CPD{n} = dbnet.CPD{n};
-    end
-    bnetEngine = jtree_inf_engine(bnet);
-    tempBnet = bnetEngine;
-    bnetEvidence = cell(1,dbnet.nnodes_per_slice);
-
-    %do inference on activity for each possible hidden node and weight results
-    %according to probability of that hidden node
-    cumulatedPred = zeros(dbnet.node_sizes_slice(2),1);
-    for c=1:dbnet.node_sizes_slice(1)
-        bnetEngine = tempBnet;
-        bnetEvidence{1} = c;
-        [bnetEngine, loglik] = enter_evidence(bnetEngine, bnetEvidence);
-        mA = marginal_nodes(bnetEngine, 2);
-        weightedPred = (mA.T *mS.T(c));
-        cumulatedPred = cumulatedPred + weightedPred;
-    end
-    % cumulatedPred auf = 1 normalisieren?
-    [M I] = max(cumulatedPred);
+    [M I] = max(mA.T);
     prediction{j} = I;
     %@Matthias, how to properly construct this cell of different activity
     %predictions?
-    prediction_probabilities{j}(:,j) = cumulatedPred; 
+    prediction_probabilities{j}(:,j) = mA.T; 
     real_value{j} = evidence{j}{2,T-1+steps};
 end
 disp('Prediction Finished');
